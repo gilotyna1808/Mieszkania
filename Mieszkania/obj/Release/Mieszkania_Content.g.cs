eaec841d9eddb9f242c54126826ea8b76021fa3d
Katarzyna Norys <katanor457@student.polsl.pl>
2020-05-14 12:46:58 +0200

    v5

--- Mieszkania/obj/Release/Mieszkania_Content.g.cs
+++ Mieszkania/obj/Release/Mieszkania_Content.g.cs
@@ -14,5 +14,10 @@
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("dostepprac.tt")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mieszkania.mdf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mieszkania_log.ldf")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sqlservertypes/readme.htm")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sqlservertypes/x64/msvcr120.dll")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sqlservertypes/x64/sqlserverspatial140.dll")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sqlservertypes/x86/msvcr120.dll")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sqlservertypes/x86/sqlserverspatial140.dll")]
 
 
