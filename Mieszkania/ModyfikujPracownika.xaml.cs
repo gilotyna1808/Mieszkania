acba485c7592187530f42e056cfe1c2c827fc567
Katarzyna Norys <katanor457@student.polsl.pl>
2020-05-14 14:09:16 +0200

    Dodaje dodaj pracowinka

---
+++ Mieszkania/ModyfikujPracownika.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mieszkania
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ModyfikujPracownika.xaml
+    /// </summary>
+    public partial class ModyfikujPracownika : Window
+    {
+        public ModyfikujPracownika()
+        {
+            InitializeComponent();
+        }
+    }
+}
