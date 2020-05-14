c7576ad9e43f01b578b438edd92db7ee8bba54cf
Katarzyna Norys <katanor457@student.polsl.pl>
2020-05-14 14:29:32 +0200

    Dodaje modyfikacje pracownika

--- Mieszkania/ModyfikujPracownika.xaml.cs
+++ Mieszkania/ModyfikujPracownika.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mieszkania.Wyswietlanie;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,38 @@ namespace Mieszkania
         public ModyfikujPracownika()
         {
             InitializeComponent();
+        }
+
+        private void btn_Wybierz_Click(object sender, RoutedEventArgs e)
+        {
+            WyswietlPrac wp = new WyswietlPrac();
+            wp.ShowDialog();
+            int temp_id = 0;
+            if(temp_id != 0)
+            {
+                txt_id.Text = Convert.ToString(temp_id);
+            }
+        }
+
+        private void btn_Modyfikuj_Click(object sender, RoutedEventArgs e)
+        {
+            int temp_id = Convert.ToInt32(txt_id.Text);
+            using(DostepPrac dp = new DostepPrac())
+            {
+                var q = from data in dp.Pracownicy
+                             orderby data.IdPracownika
+                             select data;
+                foreach(Pracownicy p in q)
+                {
+                    if(p.IdPracownika == temp_id)
+                    {
+                        p.Imie = txt_imiePrac.Text;
+                        p.Nazwisko = txt_nazPrac.Text;
+                        p.NrTel = txt_telPrac.Text;
+                    }
+                }
+                dp.SaveChanges();
+            };
         }
     }
 }
