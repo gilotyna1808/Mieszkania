3e615895c9800ac8fbe786c6f2e9984e62b01e35
gilotyna1808 <daniel1808@tlen.pl>
2020-05-17 15:22:02 +0200

    WalidacjaLokatora

---
+++ Mieszkania/Walidacja.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Mieszkania
+{
+    class Walidacja
+    {
+        public bool sprawdzImie(string s)
+        {
+            Regex imie= new Regex(@"[A-z]+");
+            if (imie.IsMatch(s))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool sprawdzNazwisko(string s)
+        {
+            Regex nazw = new Regex(@"([A-z]+.?)+");
+            if (nazw.IsMatch(s))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool sprawdzTelefon(string s)
+        {
+            Regex tel = new Regex(@"[+]?[0-9]{9,12}");
+            if (tel.IsMatch(s))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
